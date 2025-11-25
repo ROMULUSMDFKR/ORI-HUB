666a588f3df8cb47a69dc35c4a27f511a71d6543
ROMULUSMDFKR <robertoortegaonline@gmail.com>
2025-11-25 17:49:40 -0600

    feat(layout): Increase sidebar logo size
    Enlarges the ORI logo in the primary sidebar from `w-3` to `w-6` to improve visibility and branding.

--- components/layout/PrimarySidebar.tsx
+++ components/layout/PrimarySidebar.tsx
@@ -78,7 +78,7 @@ const PrimarySidebar: React.FC<PrimarySidebarProps> = ({ user }) => {
   return (
     <div className="w-20 bg-indigo-950 flex-shrink-0 flex flex-col items-center py-4 relative z-50">
       <Link to="/" className="mb-6 flex-shrink-0">
-        <img src="https://firebasestorage.googleapis.com/v0/b/ori-405da.firebasestorage.app/o/Logo%2FLOGO%20BLANCO.png?alt=media&token=88ceaa56-56c5-4270-80b4-25c0b398da84" alt="ORI Logo" className="w-3 h-auto" />
+        <img src="https://firebasestorage.googleapis.com/v0/b/ori-405da.firebasestorage.app/o/Logo%2FLOGO%20BLANCO.png?alt=media&token=88ceaa56-56c5-4270-80b4-25c0b398da84" alt="ORI Logo" className="w-6 h-auto" />
       </Link>
       <nav className="flex-1 flex flex-col items-center space-y-2 w-full px-2">
         {primaryNavLinks.map((link) => {
